f3e6844a8a6a544ebf5455cd23dd5fe9c7c42cbe
Minh-Hưng VŨ <hung.vminh17@gmail.com>
2021-03-03 17:24:55 +0700

    [ADD] Dataset folding

--- src/utils.py
+++ src/utils.py
@@ -1,15 +1,20 @@
 import os
 import json
 import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import StratifiedKFold
 # from detectron2.data import MetadataCatalog, DatasetCatalog
 
 class Dataset:
-    def __init__(self, name, annotation_path, data_path=None):
+    def __init__(self, name, annotation_path=None, data_path=None):
         self.name = name
         self.annotation = annotation_path
         self.data_path = data_path
     
     def __get_dict(self):
+        '''
+        Load annotations from JSON file
+        '''
         with open(self.annotation) as f:
             anns = json.load(f)
             return anns
@@ -20,6 +25,10 @@ class Dataset:
 #         dr_lesions_metadata = MetadataCatalog.get("dr_lesions_" + self.name)
 
     def __bin_to_dec(self, stats):
+        '''
+        Convert lesions binary list (LSB first) to decimal represented lesions combinations
+        
+        '''
         result = np.zeros((len(stats)), dtype=int)
         for idx, row in enumerate(stats):
             for order, element in enumerate(row):
@@ -27,8 +36,11 @@ class Dataset:
         return result
 
     def __key_to_lesion(self, key):
+        '''
+        Convert decimal key to lesions combinations
+        '''
         lesion = {
-            0: 'no_lesion',
+            0: 'unlabeled',
             1: 'hemorrhage',
             2: 'exudate',
             3: 'hemorrhage_exudate',
@@ -39,17 +51,63 @@ class Dataset:
         }
         return lesion.get(key)
     
+    def histogram(self, data, titles=None):
+        '''
+        Plot histogram
+        '''
+        fig, axs = plt.subplots(1, 1,sharey=True, tight_layout=True)
+        axs.hist(data, bins=np.arange(data.min(), data.max()+1))
+        if titles is not None and len(titles) == 3:
+            axs.set_title(titles[0])
+            axs.set_xlabel(titles[1])
+            axs.set_ylabel(titles[2])
+            
+        
+    def sampling(self, scale=0.8, threshold = 8):
+        '''
+        Sample dataset by two rules:
+            - Keep distribution of lesions combinations
+            - Balance amount of lesions per class.
+        Input: 
+            - scale: splitting scale of train_set/test_set 
+            - threshold: an integer separates a list of images into 2 subgroup: 'few lesions', 'many lesions'
+        *threshold = 8 gives the minimum variance of the amount of lesions after folding.
+        '''
+        lesions_filenames = [x for x in os.listdir('{}/images'.format(self.data_path)) if x[:4] == 'CTEH']
+        lesions_count = np.zeros((len(lesions_filenames),3), dtype=int)
+        
+        annotations = self.__get_dict()
+        for idx, image in enumerate(annotations):
+            lesions = image['annotations']
+            for lesion in lesions:
+                lesions_count[idx, lesion['category_id']] += 1
+                
+        image_id = [annotation['image_id'] for annotation in annotations]
+        count_mask = (lesions_count.sum(axis=1) >= threshold)*1
+        ratio = int(scale/(1 - scale)) + 1
+        skf = StratifiedKFold(n_splits=ratio)
+        folds = []
+        for train_index, test_index in skf.split(image_id, count_mask):
+            train_set = np.array(annotations)[train_index]
+            test_set = np.array(annotations)[test_index]
+            folds.append([train_set, test_set])
+            
+        return folds
+    
     def stats(self):
+        '''
+        General dataset statistics
+        '''
         lesions_filenames = [x for x in os.listdir('{}/images'.format(self.data_path)) if x[:4] == 'CTEH']
         normal_filenames  = [x for x in os.listdir('{}/images/normal'.format(self.data_path)) if x[:4] == 'CTEH']
         
         lesions_count = np.zeros((len(lesions_filenames),3), dtype=int)
-        with open(self.annotation) as f:
-            annotations = json.load(f)
-            for idx, image in enumerate(annotations):
-                lesions = image['annotations']
-                for lesion in lesions:
-                    lesions_count[idx, lesion['category_id']] += 1
+        annotations =  self.__get_dict()
+
+        for idx, image in enumerate(annotations):
+            lesions = image['annotations']
+            for lesion in lesions:
+                lesions_count[idx, lesion['category_id']] += 1
         
         lesions_combination = self.__bin_to_dec((lesions_count > 0)*1)
         unique, counts = np.unique(lesions_combination, return_counts=True)
