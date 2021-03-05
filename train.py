7f4fdbd82db208a593bdbb01f87d2284ab75b6da
Minh-Hưng VŨ <hung.vminh17@gmail.com>
2021-03-05 10:53:03 +0700

    [HOTFIX] missing libs in train.py and config

--- train.py
+++ train.py
@@ -5,6 +5,7 @@ setup_logger()
 from detectron2 import model_zoo
 from detectron2.config import get_cfg
 from detectron2.engine import DefaultTrainer
+from detectron2.data import DatasetCatalog, MetadataCatalog
 
 import numpy as np
 import os, json, cv2, argparse
@@ -14,17 +15,17 @@ from src.utils import Dataset
 parser = argparse.ArgumentParser()
 parser.add_argument('--data', help='diabetic retinopathy lesions folder', default='./data')
 parser.add_argument('--annotation', help='annotation file', default='./data/annotations.json')
-parser.add_argument('--params', help='training params file', default='./params.ini')
+parser.add_argument('--config', help='training config file', default='./config.ini')
 parser.add_argument('--output', help='output folder', default='./output')
 
 args = parser.parse_args()
 annotation_path = args.annotation
 data_path = args.data
-config_path = args.params
+config_path = args.config
 output = args.output
 
-params = ConfigParser()
-params.read(config_path)
+config = ConfigParser()
+config.read(config_path)
 
 # Sampling and registering dataset
 def config_detectron(params):
@@ -50,8 +51,7 @@ def config_detectron(params):
     return cfg
 
 def get_fold(index):
-    dataset = Dataset('dr_lesions', annotation_path, data_path)
-    folds = dataset.sampling()
+
     return folds[index]
 
 
@@ -77,10 +77,10 @@ if __name__ == '__main__':
     dr_lesions_metadata = MetadataCatalog.get('dr_lesions_train')
     
     # Load detectron config
-    config = config_detectron(params)
+    detectron2_config = config_detectron(config)
     
     # Train program
-    trainer = DefaultTrainer(config) 
+    trainer = DefaultTrainer(detectron2_config) 
     trainer.resume_or_load(resume=False)
     trainer.train()
 
